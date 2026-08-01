84804c612f9b34e706b565d042d64aa655103682
itzArka1407 <arkaprovodas2004@gmail.com>
2026-08-01 08:58:07 +0530

    Removed the duplicate tests previously done in main fn. All tests are to be conducted in test functions inside a test module

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,4 @@
-use anyhow::Result;
-use candle_core::{Device, Tensor};
-
 #[cfg(test)]
 mod tests;
 
-fn main() -> Result<()> {
-    // FIXME: For now, using CPU because the host machine doesn't have cuda-toolkit
-    let a = Tensor::new(&[[1., 2.], [3., 4.], [4., 6.]], &Device::Cpu)?;
-    let b = Tensor::new(&[[5., 6., 7.], [7., 8., 1.]], &Device::Cpu)?;
-    let c = Tensor::new(100., &Device::Cpu)?;
-
-    let g: Vec<Vec<f64>> = a.to_vec2()?;
-    println!("{}", c.to_scalar::<f64>()?);
-    println!("{g:#?}");
-    println!("{:?}", a.shape()); // Shape: [length, dimension depth]
-
-    let s = a.reshape((6, 1))?;
-    println!("s: {s:?}");
-
-    let c = a.matmul(&b)?;
-    println!("{c:?}");
-    Ok(())
-}
+fn main() {}
